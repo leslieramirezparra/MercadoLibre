1aa78a4592a152dfdf04a91e8e7423732466a4f5
leslieramirezparra <leslieramirezparra@gmail.com>
2024-02-23 08:22:47 -0600

    progress

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    // use UploadFile;
+
+    // public function home()
+    // {
+    //     $books=Book::with('author','category','file')->whereHas('category')->get();
+    //     return view('index',compact('books'));
+    // }
+
+    public function index(Request $request)
+    {
+        $products = Product::get();
+        // $books=Book::with('author','category', 'file')->whereHas('category')->get();
+        if(!$request->ajax()) return view('products.index',compact('products'));
+            return response()->json(['products'=> $products],200);
+    }
+
+    // public function store(BookRequest $request)
+    // {
+    //     try{
+    //         DB::beginTransaction();
+    //         $book=new Book($request->all());
+    //         $book->save();
+    //         $this->uploadFile($book, $request);
+    //         DB::commit();
+    //         if(!$request->ajax()) return back()->with('success','Book created');
+    //             return response()->json(['status'=>'Book created','book'=>$book],201);
+    //     } catch (\Throwable $th){
+    //         DB::rollback();
+    //         throw $th;
+    //     }
+    // }
+    // public function show(Request $request, Book $book)
+    // {
+    //     if(!$request->ajax()) return view();
+    //     return response()->json(['book'=>$book],200);
+    // }
+    // public function update(BookUpdateRequest $request, Book $book)
+    // {
+    //     try{
+    //         DB::beginTransaction();
+    //         $book->update($request->all());
+    //         $this->uploadFile($book, $request);
+    //         DB::commit();
+    //         if(!$request->ajax()) return back()->with('success','Book update');
+    //             return response()->json([],204);
+    //         } catch (\Throwable $th){
+    //             DB::rollback();
+    //             throw $th;
+    //         }
+    // }
+    // public function destroy(Request $request, Book $book)
+    // {
+    //     $book->delete();
+    //     $this->deleteFile($book);
+    //     if(!$request->ajax()) return back()->with('success','Book deleted');
+    //         return response()->json([],204);
+    // }
 }
